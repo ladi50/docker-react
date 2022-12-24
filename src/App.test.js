c1308753b622e267804450b0be3599ac54c075b9
Your Name <ladi50@walla.com>
2022-12-24 10:21:23 +0200

    add .travis.yml

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("'App' className has only one assertion", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appElements = document.getElementsByClassName("App");
+  expect(appElements).toHaveLength(0);
 });
